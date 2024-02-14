baaa72bd115feb59ed062b8ad790cc209680e48c
Stephan Schwab <sns@caimito.net>
2024-02-15 00:23:49 +0100

    more template

--- test/server.test.js
+++ test/server.test.js
@@ -2,17 +2,10 @@ import { test } from 'vitest'
 import request from 'supertest'
 import app from '../src/server.js'
 
-test('GET /', async ({ expect }) => {
-  const response = await request(app).get('/')
-  expect(response.status).toBe(200)
-  expect(response.text).toContain('Hello, World!')
-})
-
 test('GET / with referer', async ({ expect }) => {
   const response = await request(app)
     .get('/')
     .set('referer', 'http://example.com')
   expect(response.status).toBe(200)
-  expect(response.text).toContain('Hello, World!')
   expect(response.text).toContain('http://example.com')
 })
