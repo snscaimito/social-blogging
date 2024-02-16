cc8e61f82f7cdc2241f0b35dc1cb316e4b78c03f
Stephan Schwab <sns@caimito.net>
2024-02-16 13:55:11 +0100

    port

--- src/server.js
+++ src/server.js
@@ -3,7 +3,7 @@ import morgan from 'morgan'
 import { setupDatabase, addComment, findComments } from './database.js'
 
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000
 
 app.use(morgan('dev'))
 app.use(express.urlencoded({ extended: true }))
