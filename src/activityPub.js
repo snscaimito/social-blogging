c6e5eca4e86f5186f1ff27719925fb31ec9c16e6
Stephan Schwab <sns@caimito.net>
2024-02-23 14:20:02 +0100

    configuration

--- src/activityPub.js
+++ src/activityPub.js
@@ -2,39 +2,39 @@ import { isDevelopmentMode, formatMessage } from './helpers.js'
 import { savePost } from './database.js'
 
 function doPost (message) {
-  if (process.env.ENABLE_ACTIVITY_PUB === 'true') {
-    const messageText = formatMessage(message, process.env.ACTIVITY_PUB_CHARACTER_LIMIT || 500)
-
-    if (isDevelopmentMode()) {
-      console.log('Test mode, skipping post to ACTIVITY_PUB:', messageText)
-      return Promise.resolve('Test mode')
-    }
-
-    return fetch(process.env.ACTIVITY_PUB_URL, {
-      method: 'POST',
-      headers: {
-        Authorization: `Bearer ${process.env.ACTIVITY_PUB_ACCESS_TOKEN}`,
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify({ status: messageText })
-    })
-      .then(response => {
-        if (!response.ok) {
-          return Promise.reject(new Error(`Error: ${response.status}`))
-        }
-        return response.json()
-      })
-      .then(data => {
-        return data
-      })
-      .catch(error => {
-        console.error('Failed to post content:', error)
-        return Promise.reject(error)
-      })
-  } else {
+  if (!process.env.ENABLE_ACTIVITY_PUB) {
     console.log('Fediverse is disabled')
     return Promise.resolve('Fediverse is disabled')
   }
+
+  const messageText = formatMessage(message, process.env.ACTIVITY_PUB_CHARACTER_LIMIT || 500)
+
+  if (isDevelopmentMode()) {
+    console.log('Test mode, skipping post to ACTIVITY_PUB:', messageText)
+    return Promise.resolve('Test mode')
+  }
+
+  return fetch(process.env.ACTIVITY_PUB_URL, {
+    method: 'POST',
+    headers: {
+      Authorization: `Bearer ${process.env.ACTIVITY_PUB_ACCESS_TOKEN}`,
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({ status: messageText })
+  })
+    .then(response => {
+      if (!response.ok) {
+        return Promise.reject(new Error(`Error: ${response.status}`))
+      }
+      return response.json()
+    })
+    .then(data => {
+      return data
+    })
+    .catch(error => {
+      console.error('Failed to post content:', error)
+      return Promise.reject(error)
+    })
 }
 
 function postToActivityPub (message) {
